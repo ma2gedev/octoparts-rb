1cd4dbdb2ec071d8ac531f0917e6844ec88a0d25
Takayuki Matsubara <takayuki.1229@gmail.com>
2015-01-04 23:53:24 +0900

    add invalidate cache api

--- lib/octoparts/client.rb
+++ lib/octoparts/client.rb
@@ -1,5 +1,10 @@
+require 'uri'
+
 module Octoparts
   class Client
+    OCTOPARTS_API_ENDPOINT_PATH = '/octoparts/2'
+    CACHE_API_ENDPOINT_PATH = "#{OCTOPARTS_API_ENDPOINT_PATH}/cache"
+
     def initialize(host: nil)
       @host = host || 'http://localhost:9000'
     end
@@ -16,7 +21,7 @@ module Octoparts
     def invoke(params, header = {})
       body = params.to_json # TODO
       header[:content_type] = 'application/json'
-      resp = post("/octoparts/2", body, header)
+      resp = post(OCTOPARTS_API_ENDPOINT_PATH, body, header)
       Response.new(
         Model::AggregateResponse.new.extend(Representer::AggregateResponseRepresenter).from_json(resp.body),
         resp.headers,
@@ -24,7 +29,37 @@ module Octoparts
       )
     end
 
+    # TODO: doc
+    def invalidate_cache(part_id, param_name: nil, param_value: nil)
+      cache_path = if param_name
+        "/invalidate/part/#{part_id}/#{param_name}/#{param_value}"
+      else
+        "/invalidate/part/#{part_id}"
+      end
+      post_cache_api(cache_path)
+    end
+
+    # TODO: doc
+    def invalidate_cache_group(cache_group_name, param_value: nil)
+      cache_path = if param_value
+        "/invalidate/cache-group/#{cache_group_name}/params/#{param_value}"
+      else
+        "/invalidate/cache-group/#{cache_group_name}/parts"
+      end
+      post_cache_api(cache_path)
+    end
+
     private
+
+    def post_cache_api(path)
+      escaped_path = URI.escape("#{CACHE_API_ENDPOINT_PATH}#{path}")
+      resp = post(escaped_path, nil, nil)
+      Response.new(
+        resp.body,
+        resp.headers,
+        resp.status
+      )
+    end
 
     def process(method, path, params, headers)
       @connection ||= Faraday.new(url: @host) do |connection|
